afd053de5bb9e9cb9a81288fc2817f1394957367
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2009-09-05 09:07:59 +0000

    Import of KUNIYOSHI/WWW-NicoSound-Download-0.02 from CPAN.
    gitpan-cpan-distribution: WWW-NicoSound-Download
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         KUNIYOSHI/WWW-NicoSound-Download-0.02.tar.gz
    gitpan-cpan-author:       KUNIYOSHI
    gitpan-cpan-maturity:     released

--- lib/WWW/NicoSound/Download.pm
+++ lib/WWW/NicoSound/Download.pm
@@ -1,6 +1,5 @@
 package WWW::NicoSound::Download;
 
-use 5.010000;
 use strict;
 use warnings;
 use Carp;
@@ -18,7 +17,7 @@ our %EXPORT_TAGS = ( 'all' => [ qw(
 our @EXPORT_OK   = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT      = ( );
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 our $IS_RIOT = 0;
 
 my $INFO_URL  = "http://nicosound.anyap.info/sound/";
